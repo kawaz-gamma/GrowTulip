26bd1a9de7e4594b35a1dea6f2cd0371301728e0
SONO <sono.stream8580@gmail.com>
2021-09-11 22:59:06 +0900

    Add per time counter

--- Assets/Scripts/KyuukonCountPerTime.cs
+++ Assets/Scripts/KyuukonCountPerTime.cs
@@ -10,22 +10,37 @@ class KyuukonCountPerTime
     const int MAX_COUNT = 1000;
     LinkedList<KyuukonCountUnit> list;
     float countIntervalSec;
+    float updateIntervalSec;
+    float prevTime;
+    float currentCount;
+
+    public KyuukonCountPerTime(float countIntervalSec, float updateIntervalSec)
+    {
+        this.countIntervalSec = countIntervalSec;
+        this.updateIntervalSec = updateIntervalSec;
+        list = new LinkedList<KyuukonCountUnit>();
+        prevTime = Time.time;
+        currentCount = 0;
+    }
 
     public float GetCount(int kyuukonTotalCount)
     {
         float currentTime = Time.time;
+        if (currentTime - prevTime < updateIntervalSec)
+        {
+            return currentCount;
+        }
+
         while (list.Count >= MAX_COUNT || (list.Count > 0 && list.First().Time < currentTime - countIntervalSec))
         {
             list.RemoveFirst();
         }
 
         list.AddLast(new KyuukonCountUnit(kyuukonTotalCount, currentTime));
-        return kyuukonTotalCount - list.First().Count;
-    }
 
-    public KyuukonCountPerTime(float countIntervalSec)
-    {
-        this.countIntervalSec = countIntervalSec;
+        prevTime = currentTime;
+        currentCount = (kyuukonTotalCount - list.First().Count) / countIntervalSec;
+        return currentCount;
     }
 }
 
